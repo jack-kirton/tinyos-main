653b52df85632e9e2c55e30ee9d196de72ffdd1c
Matthew Bradbury <matt-bradbury@live.co.uk>
2016-01-25 10:52:46 +0000

    Better const correctness in sim_log

--- tos/lib/tossim/sim_log.h
+++ tos/lib/tossim/sim_log.h
@@ -63,14 +63,14 @@ extern "C" {
 #endif
 
 void sim_log_init(void);
-void sim_log_add_channel(char* output, FILE* file);
-bool sim_log_remove_channel(char* output, FILE* file);
+void sim_log_add_channel(const char* output, FILE* file);
+bool sim_log_remove_channel(const char* output, FILE* file);
 void sim_log_commit_change(void);
 
-void sim_log_debug(uint16_t id, char* string, const char* format, ...);
-void sim_log_error(uint16_t id, char* string, const char* format, ...);
-void sim_log_debug_clear(uint16_t id, char* string, const char* format, ...);
-void sim_log_error_clear(uint16_t id, char* string, const char* format, ...);
+void sim_log_debug(uint16_t id, const char* string, const char* format, ...);
+void sim_log_error(uint16_t id, const char* string, const char* format, ...);
+void sim_log_debug_clear(uint16_t id, const char* string, const char* format, ...);
+void sim_log_error_clear(uint16_t id, const char* string, const char* format, ...);
 
 #ifdef __cplusplus
 }
